10696a94e9912acc22ece98823046ed48785b3fc
SamuelAsaffe <samuelasaffe1@gmail.com>
2023-05-16 16:27:40 -0300

    criando

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-function Helloworld() {
-    alert("helloworld");
+function soma(n1, n2) {
+    return n1 + n2
+}
+function subtrair(n1, n2) {
+    return n1 - n2
+}
+function dividir(n1, n2) {
+    returnn1 / n2
+}
+function vezes(n1, n2) {
+    return n1 * n2
+}
+function mostrarnome(nome) {
+    let nome = prompt("qual o seu nome ?")
+    alert("olá " + nome)
 }
