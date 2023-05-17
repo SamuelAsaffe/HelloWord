cdc1f75031b10f71801d65467e984b4fa17c3c88
SamuelAsaffe <samuelasaffe1@gmail.com>
2023-05-17 15:15:46 -0300

    funcionou calculo

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
+
+
 function soma(n1, n2) {
-    alert(n1 + n2)
+    res = n1 + n2
+    alert(res)
 }
 function subtrair(n1, n2) {
     alert(n1 - n2)
 }
 function divisao(n1, n2) {
-    return n1 / n2
+    alert(n1 / n2)
 }
 function multiplicacao(n1, n2) {
-    return n1 * n2
+    alert(n1 * n2)
 }
-function mostrarnome(nome) {
+function mostrarnome() {
     let nome = prompt("qual o seu nome ?")
     alert("olá " + nome)
 }
